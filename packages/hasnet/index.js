18274680cd11529adf197921a85f66932db4093a
Vishnu Bharathi <vishnu@hasura.io>
2023-08-29 23:04:26 +0530

    add commander npm package

--- packages/hasnet/index.js
+++ packages/hasnet/index.js
@@ -1,3 +1,17 @@
 #!/bin/env node
 
-console.log('hello world!')
+const { Command } = require('commander');
+const program = new Command();
+
+program
+  .name('hasnet')
+  .description('CLI for the HasNeT stack')
+  .version('0.1.0'); // TODO: get this from package.json file
+
+program.command('init')
+  .description('initialize the hasnet-stack in current diectory')
+  .action(() => {
+    console.log('hasnet-stack will be initialized here')
+  });
+
+program.parse();
